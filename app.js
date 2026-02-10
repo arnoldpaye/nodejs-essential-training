a02059098564574dd88cd178446bddb2246ec088
Arnold Paye <arnoldpayet@gmail.com>
2026-02-10 05:29:26 -0400

    exporting custom modules

---
+++ app.js
@@ -0,0 +1,10 @@
+// const myModule = require("./myModule");
+const { inc, dec, getCount } = require("./myModule");
+
+// console.log(myModule.anything);
+// console.log(myModule.inc());
+
+inc();
+inc();
+inc();
+console.log(`the count is ${getCount()}`);
