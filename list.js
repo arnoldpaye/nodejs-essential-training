f58352aabf60615a4215c9b29690ab9030904dfd
Arnold Paye <arnoldpayet@gmail.com>
2026-02-11 05:20:20 -0400

    listing directory files

---
+++ list.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+
+// let files = fs.readdirSync("./");
+fs.readdir("./", function (err, files) {
+  if (err) {
+    throw err;
+  }
+  console.log(files);
+});
+
+console.log("reading files...");
